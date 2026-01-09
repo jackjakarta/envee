18507b81d51a94c43e9bc9306090eecda45d057e
jackjakarta <al.termure@gmail.com>
2026-01-09 04:03:13 +0100

    feat: add build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+GOOS=linux GOARCH=amd64 go build -o envee && tar -czf envee-linux-amd64.tar.gz envee && \
+GOOS=linux GOARCH=arm64 go build -o envee && tar -czf envee-linux-arm64.tar.gz envee && \
+GOOS=darwin GOARCH=amd64 go build -o envee && tar -czf envee-darwin-amd64.tar.gz envee && \
+GOOS=darwin GOARCH=arm64 go build -o envee && tar -czf envee-darwin-arm64.tar.gz envee
+
+rm envee
+echo "Build complete!"
+echo "Checksums:"
+
+shasum -a 256 envee-darwin-arm64.tar.gz
+shasum -a 256 envee-darwin-amd64.tar.gz
+
+shasum -a 256 envee-linux-arm64.tar.gz
+shasum -a 256 envee-linux-amd64.tar.gz
